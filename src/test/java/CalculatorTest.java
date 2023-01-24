80db270006d9d0d70026239cf3239491fe6f5108
brunbs <babosa227@gmail.com>
2023-01-24 11:03:28 -0300

    Refactor - test names with best practices and display names

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,18 +1,22 @@
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@DisplayName("Test Math Operations in Calculator Class")
 public class CalculatorTest {
 
+    @DisplayName("Test 4/2 = 2")
     @Test
-    void integerDivision() {
+    void testIntegerDivision_WhenFourIsDividedByTwo_ShouldReturnTwo() {
         Calculator calculator = new Calculator();
         int result = calculator.integerDivision(4,2);
         assertEquals(2, result, "4 / 2 should result in 2");
     }
 
+    @DisplayName("Test 10-5 = 5")
     @Test
-    void integerSubtraction() {
+    void integerSubtraction_WhenTenMinusFive_ShouldReturnFive() {
         Calculator calculator = new Calculator();
         int minuend = 10;
         int subtrahend = 5;
